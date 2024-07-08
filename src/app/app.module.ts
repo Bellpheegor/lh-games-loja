3e86b8b349719ef5efe2c32fe828d68fe4005447
Wesley Rodrigues <id_wesley@hotmail.com>
2024-07-07 23:20:32 -0300

    adicao do menu restrito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,20 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RestritoRoutingModule } from './restrito/restrito-routing-module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { RestritoComponent } from './restrito/restrito.component';
+import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.component';
+import { ListaProdutoComponent } from './restrito/lista-produto/lista-produto.component';
+import { CadastroProdutoComponent } from './restrito/cadastro-produto/cadastro-produto.component';
+import { AtualizaProdutoComponent } from './restrito/atualiza-produto/atualiza-produto.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -29,6 +37,11 @@ import { RodapeComponent } from './rodape/rodape.component';
     LoginComponent,
     MenuComponent,
     RodapeComponent,
+    RestritoComponent,
+    MenuRestritoComponent,
+    ListaProdutoComponent,
+    CadastroProdutoComponent,
+    AtualizaProdutoComponent,
 
   ],
   imports: [
@@ -42,7 +55,11 @@ import { RodapeComponent } from './rodape/rodape.component';
     MatIconModule,
     MatInputModule,
     MatMenuModule,
-    MatToolbarModule
+    MatToolbarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RestritoRoutingModule,
 
   ],
   providers: [],
